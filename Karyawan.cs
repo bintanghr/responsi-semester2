78eac7a0a27afe693e6c5016f4b28771821ad133
bintanghr <haridaramadhan@gmail.com>
2022-04-19 16:31:57 +0700

    program data karyawan sederhana

---
+++ Karyawan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4325
+{
+    internal class Karyawan
+    {
+        private int nik;
+        private string nama;
+        private float gajibulanan;
+
+        public int Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public float GajiBulanan
+        {
+            get { return gajibulanan; }
+            set { gajibulanan = value; }
+        }
+
+        public Karyawan(int nik, string nama, float gajibulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            this.gajibulanan = gajibulanan;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4325
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan(40002, "Indisya", 2400000);
+            Karyawan karyawan2 = new Karyawan(30001, "Sismita", 2800000);
+
+            Console.WriteLine("No  Nik    Nama     Gaji Bulanan");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine("1. {0}  {1}  {2}", karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine("2. {0}  {1}  {2}", karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+            Console.WriteLine("Horeeeee ! ada kenaikan gaji 10%");
+
+            Console.ReadKey();
+        }
+    }
+}
